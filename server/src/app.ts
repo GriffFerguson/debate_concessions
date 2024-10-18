d270b38b2daac88e8ff61c63da28b74515cee125
Griffin Ferguson <griffinferguson.us@gmail.com>
2024-10-18 02:49:59 -0500

    implementation of retrieving payment data

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,22 @@
 import Express from "express";
 import BodyParser from "body-parser";
+import Stripe from "stripe";
+require("dotenv").config({path: "../.env"});
+
 
 const app = Express();
+const stripe = new Stripe(process.env.STRIPE_KEY!);
+
 app.use(BodyParser.text());
 app.use(BodyParser.json());
 
-app.post("/order", (req, res) => {
-    console.log(req.body);
+app.post("/order", async (req, res) => {
+    const order_data = req.body.data.object.custom_fields;
+    const id = req.body.data.object.id;
+    
+    const line_items = stripe.checkout.sessions.retrieve(id, {expand: ["line_items"]});
+
+    console.log(order_data, id, line_items);
 })
 
 app.listen(4000);
